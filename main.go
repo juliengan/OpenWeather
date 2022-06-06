fef1fa8a057ff4a2d5aab2ec4e91f27309cfad8b
juliengan <38871210+juliengan@users.noreply.github.com>
2022-06-07 01:02:24 +0200

    ENV_VAR added for location

--- main.go
+++ main.go
@@ -4,19 +4,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	owm "github.com/briandowns/openweathermap"
 )
 
 var apiKey = os.Getenv("OPENWEATHERMAP_API_KEY")
+var lat = os.Getenv("LAT")
+var long = os.Getenv("LONG")
 
 func main() {
 
-	w, err := owm.NewCurrent("F", "ru", apiKey) // fahrenheit (imperial) with Russian output
+	w, err := owm.NewCurrent("F", "EN", apiKey) // (internal - OpenWeatherMap reference for Farenheit) with English output
+	lon, err := strconv.ParseFloat(long, 64)
+	latitude, err := strconv.ParseFloat(lat, 64)
+	fmt.Println(lon)
+	fmt.Println(latitude)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	w.CurrentByCoordinates(
+		&owm.Coordinates{
+			Longitude: lon,
+			Latitude:  latitude,
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	owm.ValidAPIKey(apiKey)
 
-	w.CurrentByName("Phoenix")
 	fmt.Println(w)
+
 }
